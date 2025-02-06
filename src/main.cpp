b24a5d1b9b874ae25f6ce87a9027563b4d65ca52
skalalay <skalalay@mail.ru>
2025-02-06 04:30:55 +0300

    gitignore_7

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,11 @@
 int cnt = 0;
 
 // !.pio/build/esp32dev/firmware.bin
+// cs = 5
+// dc = 17
+// res = 16
+// sda = 23
+// sclk = 18
 
 U8G2_SSD1309_128X64_NONAME0_F_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/5, /* dc=*/17, /* reset=*/16);
 
